81d4cd5fbe60c267a9ded1db84d1a688c634f838
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-16 07:17:28 +0700

    #11 - Add Kafka Update Price Producer

---
+++ src/main/java/com/hendisantika/springbootkafkaproducermongodb/kafka/KafkaUpdatePriceProducer.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootkafkaproducermongodb.kafka;
+
+import com.hendisantika.springbootkafkaproducermongodb.model.Inventory;
+import com.hendisantika.springbootkafkaproducermongodb.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-kafka-producer-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/11/18
+ * Time: 07.14
+ */
+@Component
+public class KafkaUpdatePriceProducer implements UpdatePriceProducer {
+
+    @Autowired
+    private InventoryRepository repository;
+
+    private MessageChannel toKafka;
+
+    @Autowired
+    public KafkaUpdatePriceProducer(@Qualifier("toKafka") MessageChannel toKafka) {
+        this.toKafka = toKafka;
+    }
+
+    @Override
+    public void updateDataPriceAndPublishToKafka(String id, double price) {
+
+        Inventory inventory = repository.findById(id);
+        //check if new price < old price (diskon)
+        //send to kafka
+        if (price < inventory.getPrice()) {
+            Message<?> content = new GenericMessage<String>(Double.toString(price));
+            toKafka.send(content);
+        }
+        repository.updatePriceProduct(id, price);
+    }
+}
